3d857752cf7f6c5c5d637b90070517522d62f38c
Pedro L. <gitprojects309@gmail.com>
2026-05-09 17:41:18 -0300

    Add product entity

---
+++ src/main/java/com/ogtech/projeto_api/models/Produto.java
@@ -0,0 +1,61 @@
+package com.ogtech.projeto_api.models;
+
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "preco")
+    private BigDecimal preco;
+
+    @Column(name = "quantidade")
+    private int quantidade;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
