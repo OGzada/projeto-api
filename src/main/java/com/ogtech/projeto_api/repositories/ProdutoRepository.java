f0965a3c34d11d3fb2dd7c58b066decb0fccace0
Pedro L. <gitprojects309@gmail.com>
2026-05-09 17:41:29 -0300

    Add product repository

---
+++ src/main/java/com/ogtech/projeto_api/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.ogtech.projeto_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ogtech.projeto_api.models.Produto;
+
+@Repository
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
